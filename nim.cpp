ca5d6b5ddada1eaa5a0b845151dd2fefd52a4571
David Huynh <david_domo@hotmail.com>
2021-08-12 15:44:42 -0400

    Add Instructions and numPlayers to nim.cpp

--- nim.cpp
+++ nim.cpp
@@ -3,16 +3,25 @@
 #include <cstdlib>
 using namespace std;
 
+const int MAX_CHIPS = 50;
+
 int main()
 {
     bool p1Turn = true;
     bool gameOver = false;
 
     int totalChips = 0;
-
+    int numPlayers = 0;
     srand(time(0));
+    cout << "Instructions: The goal of Nim is to take turns removing chips from a pile.\
+    The one to take the last chip loses.\n";
+    totalChips = (rand() % MAX_CHIPS) + 1;
 
-    totalChips = (rand() % 100) + 1;
+    cout << "How many players?";
+    cin >> numPlayers;
+    string playerNames[numPlayers];
+
+    
     cout << "There are this many chips in the pile: " << totalChips << endl;
     return 0;
 }
